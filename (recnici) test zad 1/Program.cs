93e33f5c9aa97b90c3db865b1d0182258128a6c3
Denislav-I7 <denislavivanov873@gmail.com>
2025-06-19 10:19:29 +0300

    Add files via upload

---
+++ (recnici) test zad 1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _recnici__test_zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            string name;
+
+            while ((name = Console.ReadLine()) != "stop")
+            {
+                if (count.ContainsKey(name))
+                {
+                    count[name]++;
+                }
+                else
+                {
+                    count.Add(name, 1);
+                }
+            }
+
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+
+        }
+    }
+}

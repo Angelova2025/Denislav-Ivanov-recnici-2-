ef0c5a93b48e4a92e6b3ffb3736f8c89f135ebcf
Denislav-I7 <denislavivanov873@gmail.com>
2025-06-19 10:42:44 +0300

    Add files via upload

---
+++ (recnici) zad 2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _recnici__zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Dictionary<string, List<string>> Books = new Dictionary<string, List<string>>();
+            List<string> Order = new List<string>();
+            string line;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] parts = line.Split(new string[] {"-->"} , StringSplitOptions.None);
+                string Name = parts[0];
+                string title = parts[1];
+
+                if (!Books.ContainsKey(Name))
+                {
+                    Books.Add(Name, new List<string>());
+                    Order.Add(Name);
+                }
+                Books[Name].Add(title);
+            }
+            foreach (string Name in Order)
+            {
+                Console.WriteLine($"{Name}: {string.Join(",", Books[Name])}");
+            }
+        }
+    }
+}
